ca9ae6e475dd702671011a2bd3d2a98e20518685
Eric Millinder <emillinder@politico.com>
2024-01-03 17:00:47 -0500

    some server apis and an unused user

--- pages/about.vue
+++ pages/about.vue
@@ -1,10 +1,14 @@
 <script setup lang="ts">
 const count = useState('counter', () => Math.round(Math.random() * 100))
+
 const increment = () => count.value++
+
+const {data} = await useFetch('/api/hello')
 </script>
 
 <template>
-  <p @click.prevent="increment">{{count}}</p>
+  <p @click.prevent="increment">{{ count }}</p>
+  <p>Hello, {{ data["hello"] }}</p>
 </template>
 
 <style scoped>
